244afd3ec355c78456d17bca42daa9dea6088e72
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2021-08-04 13:44:40 +0530

    Update Virtual_assistant.py

--- Virtual_assistant.py
+++ Virtual_assistant.py
@@ -6,6 +6,6 @@ Created on Fri Jul 16 13:33:15 2021
 """
 
 import wolframalpha
-client= wolframalpha.Client('2KTQER-9E9RWRQQY4')
+client= wolframalpha.Client('-----------------') #Hiding client
 res=client.query("Temperature in Hyderabad on July 10, 2020")
 print(res)
